315525d7c65e37eeced6bb4a1dace94313f21ca2
TanishqDsharma <tanishqsharma612@gmail.com>
2025-03-25 16:08:53 +0530

    Core NudegeCamapign Interface

---
+++ src/campaign/interfaces/IBaseNudgeCampaign.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: MIT
+pragma solidity  ^0.8.28;
+
+
+/**
+ * This interface defines the core structure for a Nudge Campaign Contract. It provides with
+ * errors, structs, events, and functions necessary to manage user participation.
+ */
