4bee6c752a263b874f4f331fdc2acb2dff6f4f65
TanishqDsharma <tanishqsharma612@gmail.com>
2025-03-25 16:17:16 +0530

    Interface: Added errors

--- src/campaign/interfaces/IBaseNudgeCampaign.sol
+++ src/campaign/interfaces/IBaseNudgeCampaign.sol
@@ -6,3 +6,16 @@ pragma solidity  ^0.8.28;
  * This interface defines the core structure for a Nudge Campaign Contract. It provides with
  * errors, structs, events, and functions necessary to manage user participation.
  */
+
+interface IBaseNudgeCamapaign {
+
+// ERRORS
+
+error CampaignPaused();
+error UnauthorizedSwapCaller();
+error Unautorized();
+error InsufficientAmountReceived();
+error InvalidToTokenRecevied();
+
+
+}
