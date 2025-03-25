21d6b3fce64fcb9c0e58258b3ed8bb31af403baa
TanishqDsharma <tanishqsharma612@gmail.com>
2025-03-25 16:27:45 +0530

    Interface: Added Enum, Sturct and functions

--- src/campaign/interfaces/IBaseNudgeCampaign.sol
+++ src/campaign/interfaces/IBaseNudgeCampaign.sol
@@ -9,13 +9,60 @@ pragma solidity  ^0.8.28;
 
 interface IBaseNudgeCamapaign {
 
-// ERRORS
+    // ERRORS
 
-error CampaignPaused();
-error UnauthorizedSwapCaller();
-error Unautorized();
-error InsufficientAmountReceived();
-error InvalidToTokenRecevied();
+    error CampaignPaused();
+    error UnauthorizedSwapCaller();
+    error Unautorized();
+    error InsufficientAmountReceived();
+    error InvalidToTokenRecevied();
+
+    //Enums 
+
+    enum ParticipationStatus {
+        PARTICIPATING,
+        INVALIDATED,
+        CLAIM,
+        HANDLED_OFFCHAIN
+    }
+
+    // Structs
+
+    struct Participation {
+        ParticipationStatus status;
+        address userAddress;
+        uint256 toAmount;
+        uint256 userAmount;
+        uint256 startTimstamp;
+        uint256 startBlockNumber;
+    }
+
+    // Events
+
+    event NewParticipation(
+        uint256 indexed campaignId,
+        uint256 indexed userAddress,
+        uint256 pID,
+        uint256 toAmount,
+        uint256 entitledRewards,
+        uint256 fees,
+        bytes data
+    );
+
+    // functions
+
+    // external function
+    function handleReallocation(
+        uint256 campaignId,
+        address userAddress,
+        address toToken,
+        uint256 toAmount,
+        bytes memory data
+    ) external payable;
+
+    // internal function
+
+    function getBalanceOfSelf(address token) external view returns(uint256);
 
 
 }
